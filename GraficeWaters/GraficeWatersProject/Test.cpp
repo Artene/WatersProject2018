ae22da42c25c69a3c62287cb0b6d7efc1262a24b
Andrei A <40656152+Artene@users.noreply.github.com>
2018-06-28 15:20:38 -0800

    Neatly_Packed
    If not working, TOO BAD!

---
+++ GraficeWaters/GraficeWatersProject/Test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Generator.h"
+#include "Graph.h"
+#include "ProcessingUnit.h"
+
+int main()
+{
+	Generator Calin(180,0,180,20);
+	Calin.WriteRandomNumberToFile();
+	Graph Sorin("GeneratedNumbers.txt");
+	Sorin.DisplayOnScreen();
+	ProcessingUnit Izabela(Sorin,"ModifiedNumbers.txt");
+	Izabela.Sum();
+	Izabela.WriteToFile();
+	system("pause");
+	return 0;
+}
